4eabf5568ef4555888af3ad91603a0c1afb63ae9
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2023-12-07 12:11:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "f060a71819e2tbf51f08f683o95b4c96";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
